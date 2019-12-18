5843055062d24929303a169ad6a6f9457896b08c
funderburkjim <funderburkjim@gmail.com>
2019-12-17 20:45:14 -0500

    Revise so only dictionaries/xxx/xxx_correctionform.txt is revised only for dictionaries that have new correction submissions.

--- redo_cfr.sh
+++ redo_cfr.sh
@@ -2,6 +2,6 @@
 #echo "download new cfr.tsv from Cologne"
 #curl -o cfr.tsv https://www.sanskrit-lexicon.uni-koeln.de/php/correction_response/cfr.tsv
 echo "regenerate correctionform.txt and dictionaries/xxx/xxx_correctionform.txt"
-python cfr_adj.py cfr.tsv correctionform.txt
+python3 cfr_adj.py app/correction_response/cfr.tsv correctionform.txt
 echo "redo_cfr.sh is finished"
 
